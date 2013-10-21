4f6bb1dd381a299ab3d379b4255276d28f873abd
jdwelch58 <jdwelch58@gmail.com>
2013-10-21 11:40:54 -0500

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        This is just a sample index file
+        ?>
+    </body>
+</html>
